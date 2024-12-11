680a2a8f08ae773d3b6b5f615e128fd692b569e6
Roshan Baidar <roshanbaidar1234@gmail.com>
2024-12-11 11:00:46 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:calculator_app/calculator_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: CalculatorView(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
